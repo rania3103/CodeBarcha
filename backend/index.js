e861ca78bdedff55a0afe52fd7dc3ea23a6efa28
rania3103 <raniaabassi33@gmail.com>
2024-01-28 16:20:51 +0100

    modify index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
+const todoRoutes = require('./routes/todoRoutes');
+// Middleware
 app.use(express.json());
+// Routes
+app.use('/api/todo', todoRoutes);
 
 app.listen(port, () => {
   console.log('server is running');
