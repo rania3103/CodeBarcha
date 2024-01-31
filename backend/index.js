a3de47fdca9f6bb4722ce0b9b8599dfb8b6c0c89
rania3103 <raniaabassi33@gmail.com>
2024-01-31 11:19:24 +0100

    add userRoutes

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 const todoRoutes = require('./routes/todoRoutes');
+const userRoutes = require('./routes/userRoutes');
 // Middleware
 app.use(express.json());
 // Routes
 app.use('/api/todo', todoRoutes);
-
+app.use('/api/auth', userRoutes);
 app.listen(port, () => {
   console.log('server is running');
 });
