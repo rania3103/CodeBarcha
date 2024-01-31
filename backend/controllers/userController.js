e0d992c1ac9aa45a403fb16dbefc7942e6faef37
rania3103 <raniaabassi33@gmail.com>
2024-01-31 16:14:59 +0100

    add logoutUser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,9 @@
 const db = require('../db/db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+// register user
 const registerUser = (req, res) => {
   bcrypt.hash(req.body.password, 10)
     .then((hashedPwd) => {
@@ -20,4 +23,44 @@ const registerUser = (req, res) => {
       res.status(500).send('Error hashing password');
     });
 };
-module.exports = { registerUser };
+
+// login
+const loginUser = (req, res) => {
+  const { email, password } = req.body;
+  db.query('select * from app_user where email = $1', [email])
+    .then((result) => {
+      const user = result.rows[0];
+      if (!user) {
+        res.status(401).json({ message: 'Invalid email or password' });
+      } else {
+        bcrypt.compare(password, user.password)
+          .then((compPwd) => {
+            if (!compPwd) {
+              res.status(401).json({ message: 'Invalid email or password' });
+            } else {
+              // if passwords match (user is sucessfully authenticated) so we generate jwt
+              const key = process.env.SECRET_KEY;
+              const token = jwt.sign({ id: user.userid, email: user.email, githubUserName: user.githubusername },
+                key, { expiresIn: '1h' });
+              res.status(200).json({ token, expiresIn: 3600 });
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(401).send('Error comparing passwords');
+          });
+      }
+    }
+    )
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error querying database');
+    });
+};
+
+// logout
+const logoutUser = (req, res) => {
+  res.status(200).json({ message: 'Logout sucessfully' });
+};
+
+module.exports = { registerUser, loginUser, logoutUser };
