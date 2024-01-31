f9b70f6d399bac0f835f88b9baad0a0890b14e1e
rania3103 <raniaabassi33@gmail.com>
2024-01-31 11:56:26 +0100

    fix eslint

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ require('dotenv').config();
 const port = process.env.PORT;
 const todoRoutes = require('./routes/todoRoutes');
 const userRoutes = require('./routes/userRoutes');
+
 // Middleware
 app.use(express.json());
 // Routes
 app.use('/api/todo', todoRoutes);
 app.use('/api/auth', userRoutes);
+
 app.listen(port, () => {
   console.log('server is running');
 });
