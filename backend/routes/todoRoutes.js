7a27eb0f73c14594f278556d915170507862c10b
rania3103 <raniaabassi33@gmail.com>
2024-02-01 12:59:23 +0100

    add verifyToken

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoController');
+const verifyToken = require('../middlewares/verifyToken');
 
+router.use(verifyToken);
 router.post('/', todoController.createTask);
 router.get('/', todoController.getTasks);
 router.put('/:id', todoController.updateTask);
