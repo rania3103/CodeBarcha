d511ab25afd940cd262187757277a026d6ff046a
rania3103 <raniaabassi33@gmail.com>
2024-02-01 14:17:44 +0100

    add github controllers

---
+++ backend/controllers/githubController.js
@@ -0,0 +1,18 @@
+const db = require('../db/db');
+
+const getUserRepos = (req, res) => {
+
+};
+
+const getRepo = (req, res) => {
+
+};
+
+const cloneRepo = (req, res) => {
+
+};
+
+const createRepo = (req, res) => {
+
+};
+module.exports = { getUserRepos, getRepo, cloneRepo, createRepo };
