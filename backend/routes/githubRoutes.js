7f22debcdf3d5c282f5920dedc6e6b1d29c7c1a7
rania3103 <raniaabassi33@gmail.com>
2024-02-01 14:13:36 +0100

    add routes of github

--- backend/routes/githubRoutes.js
+++ backend/routes/githubRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const githubController = require('../controllers/githubController');
 
-router.post('/repos', githubController);
+router.get('/repos', githubController.getUserRepos);
+router.get('/repo/:id', githubController.getRepo);
+router.post('/clone', githubController.cloneRepo);
+router.post('/repos', githubController.createRepo);
 
 module.exports = router;
