612d2c12b24e966cb16cba594e77fce464fb43da
rania3103 <raniaabassi33@gmail.com>
2024-02-01 14:07:35 +0100

    add githubroutes

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,13 @@ require('dotenv').config();
 const port = process.env.PORT;
 const todoRoutes = require('./routes/todoRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const githubRoutes = require('./routes/githubRoutes');
 // Middleware
 app.use(express.json());
 // Routes
 app.use('/api/todo', todoRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/api/github', githubRoutes);
 
 app.listen(port, () => {
   console.log('server is running');
