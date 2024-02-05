67d09cee69d33008f2871b5e1936105fb7786ae0
rania3103 <raniaabassi33@gmail.com>
2024-02-05 13:31:57 +0100

    add get user repos

--- backend/controllers/githubController.js
+++ backend/controllers/githubController.js
@@ -1,18 +1,23 @@
-const db = require('../db/db');
+const axios = require('axios');
 
 const getUserRepos = (req, res) => {
-
+  const githubUserName = req.user.githubUserName;
+  axios.get(`https://api.github.com/users/${githubUserName}/repos?sort=updated`)
+    .then((resp) => {
+      const repos = resp.data;
+      res.status(200).json(repos);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to fetch user repos from github api' });
+    });
 };
-
-const getRepo = (req, res) => {
-
-};
-
+/*
 const cloneRepo = (req, res) => {
 
 };
 
 const createRepo = (req, res) => {
 
-};
-module.exports = { getUserRepos, getRepo, cloneRepo, createRepo };
+}; */
+module.exports = { getUserRepos };
--- backend/routes/githubRoutes.js
+++ backend/routes/githubRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const githubController = require('../controllers/githubController');
-
+const verifyToken = require('../middlewares/verifyToken');
+router.use(verifyToken);
 router.get('/repos', githubController.getUserRepos);
-router.get('/repo/:id', githubController.getRepo);
-router.post('/clone', githubController.cloneRepo);
-router.post('/repos', githubController.createRepo);
+/*router.post('/clone', githubController.cloneRepo);
+router.post('/repos', githubController.createRepo);*/
 
 module.exports = router;
