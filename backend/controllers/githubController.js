61997a11f352026d67a6c7841639781ea7c5ee43
rania3103 <raniaabassi33@gmail.com>
2024-02-11 17:03:11 +0100

    change to graphql

--- backend/controllers/githubController.js
+++ backend/controllers/githubController.js
@@ -1,17 +1,46 @@
 const axios = require('axios');
 
+// get user repos(private/public with link to clone repo)
 const getUserRepos = (req, res) => {
-  const githubUserName = req.user.githubUserName;
-  axios.get(`https://api.github.com/users/${githubUserName}/repos?sort=updated`)
+  const githubAccessToken = req.user.githubAccessToken;
+  const query = `
+  query {
+    viewer {
+      repositories(first: 50) {
+        nodes {
+          name
+          url
+          isPrivate
+        }
+      }
+    }
+  }
+`;
+
+  axios.post('https://api.github.com/graphql', { query },
+    {
+      headers: {
+        Authorization: `Bearer ${githubAccessToken}`,
+        'User-Agent': 'CodeBarcha',
+        'Content-Type': 'application/json'
+      }
+    })
     .then((resp) => {
-      const repos = resp.data;
-      res.status(200).json(repos);
+      const repos = resp.data.data.viewer.repositories.nodes;
+      const reposDetails = repos.map((repo) => ({
+        name: repo.name,
+        cloneUrl: `${repo.url}.git`,
+        visibility: repo.isPrivate ? 'private' : 'public'
+      }));
+      res.status(200).json(reposDetails);
     })
     .catch((err) => {
       console.error(err);
       res.status(500).json({ error: 'Failed to fetch user repos from github api' });
     });
 };
+
+// search public repos
 /*
 const cloneRepo = (req, res) => {
 
