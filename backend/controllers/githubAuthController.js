ceeb3226b3c313ea2f532d32aeab1addfb4e88f9
rania3103 <raniaabassi33@gmail.com>
2024-02-12 12:28:23 +0100

    fix path of file

--- backend/controllers/githubAuthController.js
+++ backend/controllers/githubAuthController.js
@@ -1,4 +1,4 @@
-const passport = require('./config/passport-config');
+const passport = require('../config/passport-config');
 
 const OAuth = passport.authenticate('github', { scope: ['user', 'repo'] });
 const OAuthCallback = passport.authenticate('github', { failureRedirect: '/' }, (req, res) => {
