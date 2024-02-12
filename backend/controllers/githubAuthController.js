f522702691e4e1c3e2457c55e6e28f11f12c7a68
rania3103 <raniaabassi33@gmail.com>
2024-02-12 12:23:17 +0100

    fix typo

---
+++ backend/controllers/githubAuthController.js
@@ -0,0 +1,8 @@
+const passport = require('./config/passport-config');
+
+const OAuth = passport.authenticate('github', { scope: ['user', 'repo'] });
+const OAuthCallback = passport.authenticate('github', { failureRedirect: '/' }, (req, res) => {
+  res.redirect('api/github/repos');
+});
+
+module.exports = { OAuth, OAuthCallback };
