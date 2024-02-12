42719b657704368f26145e50bc64bfd28c0d1b93
rania3103 <raniaabassi33@gmail.com>
2024-02-12 13:16:23 +0100

    fix callabck

--- backend/controllers/githubAuthController.js
+++ backend/controllers/githubAuthController.js
@@ -1,8 +1,10 @@
 const passport = require('../config/passport-config');
 
 const OAuth = passport.authenticate('github', { scope: ['user', 'repo'] });
-const OAuthCallback = passport.authenticate('github', { failureRedirect: '/' }, (req, res) => {
+
+const handleCallback = (req, res) => {
   res.redirect('api/github/repos');
-});
+};
+const OAuthCallback = passport.authenticate('github', { failureRedirect: '/' }, handleCallback);
 
 module.exports = { OAuth, OAuthCallback };
