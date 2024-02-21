bf5fb7d5dcc566ef630c4f55647fafacec244e39
rania3103 <raniaabassi33@gmail.com>
2024-02-21 15:32:26 +0100

    add cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const githubAuthRoutes = require('./routes/githubAuthRoutes');
 const commandsRoutes = require('./routes/commandsRoutes');
 const passport = require('./config/passport-config');
 const session = require('express-session');
-
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
@@ -14,6 +14,7 @@ const port = process.env.PORT;
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 app.use(session({ secret: process.env.SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
