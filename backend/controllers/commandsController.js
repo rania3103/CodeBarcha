49032495bce65ab3ed9313d6b2f8859508173115
rania3103 <raniaabassi33@gmail.com>
2024-02-23 09:14:34 +0100

    handle cd command

--- backend/controllers/commandsController.js
+++ backend/controllers/commandsController.js
@@ -2,12 +2,18 @@ const { exec } = require('child_process');
 
 const executeCommand = (req, res) => {
   const command = req.body.command;
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      res.status(500).json({ error: stderr });
-    } else {
-      res.json({ result: stdout });
-    }
-  });
+  if (command.startsWith('cd')) {
+    const path = command.substring(2).trim();
+    process.chdir(path);
+    res.json({ result: process.cwd() });
+  } else {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        res.status(500).json({ error: stderr });
+      } else {
+        res.json({ result: stdout });
+      }
+    });
+  }
 };
 module.exports = { executeCommand };
