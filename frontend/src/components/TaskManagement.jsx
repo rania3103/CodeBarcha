f24164e3dff4cc2b2c9809c289ce791845332f5f
rania3103 <raniaabassi33@gmail.com>
2024-03-04 16:37:01 +0100

    fix issues

--- frontend/src/components/TaskManagement.jsx
+++ frontend/src/components/TaskManagement.jsx
@@ -1,62 +1,139 @@
-import { GrUpdate } from "react-icons/gr";
-import { MdDelete } from "react-icons/md";
-import { List, ListItem, Card } from "@material-tailwind/react";
+import { Card, Typography } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from "date-fns";
 
 function TaskManagement() {
-  const [description, setDescription] = useState('');
-  const [dueDate, setDueDate] = useState('');
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState(null);
   const [tasks, setTasks] = useState([]);
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem("authToken");
+
   const handleGetTasks = async () => {
     try {
-      const res = await axios.get('http://localhost:3000/api/todo', {headers:{Authorization: `Bearer ${token}`}});
+      const res = await axios.get("http://localhost:3000/api/todo", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
       setTasks(res.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const handleAddTask = async () => {
     try {
-      const res = await axios.post('http://localhost:3000/api/todo', { description, dueDate }, {headers:{Authorization: `Bearer ${token}`}});
-      console.log(res.data);
-      setTasks((tasks) => [...tasks, res.data]);
-      setDescription('');
-      setDueDate('');
+      await axios.post(
+        "http://localhost:3000/api/todo",
+        { description, dueDate },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      handleGetTasks();
+      setDescription("");
+      setDueDate(null);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
+  const handleDeleteTask = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/api/todo/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setTasks((prevTasks) => prevTasks.filter((task) => task.taskId !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     handleGetTasks();
   }, []);
 
   return (
-    <div className="bg-gray-900 h-full">
-      <h2 className="text-center mb-5 pt-5 text-white font-bold text-xl">Task Management</h2>
+    <div className="w-full mx-auto">
+      <h2 className="text-center mb-5 pt-5 text-white font-extrabold text-xl">
+        TODO
+      </h2>
       <div className="text-center">
-        <input type="text" placeholder="Task description" className="mr-3 ml-5 py-1 px-3 bg-slate-100 rounded" value={description} onChange={(e) => setDescription(e.target.value)} />
-        <input type="text" placeholder="d/m/y" className="py-1 px-3 bg-slate-100 rounded" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
-        <button className="ml-4 bg-indigo-500 py-1 px-5 text-white font-semibold rounded" onClick={handleAddTask}>Add Task</button>
+        <input
+          type="text"
+          placeholder="Task description"
+          className="mr-3 ml-5 py-1 px-3 bg-slate-100 rounded outline-0"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <DatePicker
+          selected={dueDate}
+          onChange={(date) => setDueDate(date)}
+          className="py-1 px-3 bg-slate-100 rounded outline-0"
+          dateFormat="yyyy/MM/dd"
+          placeholderText="Due date"
+        />
+        <button
+          className="ml-4 bg-indigo-500 py-1 px-5 text-white font-semibold rounded hover:bg-indigo-300"
+          onClick={handleAddTask}
+        >
+          Add Task
+        </button>
       </div>
-      <div className="mt-6 flex justify-center">
-        <Card className="w-96 border-4 border-indigo-500/50 py-2 flex justify-center rounded">
-          <List className="bg-slate-100 rounded">
-            {tasks.map((task) => (
-              <ListItem key={task.id} className="py-1 pr-1 pl-4 ">
-                {task.description} {task.dueDate}
-                <button className="ml-6 text-green-600"><GrUpdate /> </button>
-                <button className="ml-3 text-red-600 text-xl"><MdDelete /> </button>
-              </ListItem>
-            ))}
-          </List>
+      <div className="mt-6">
+        <Card className="bg-white rounded-md overflow-y-scroll overflow-x-scroll mx-24">
+          <table className="w-full table-auto text-left">
+            <thead>
+              <tr>
+                <th className="border-b border-blue-gray-100 bg-blue-gray-50 py-2">
+                  <Typography className="font-extrabold opacity-70 text-indigo-800 text-2xl">
+                    Task
+                  </Typography>
+                </th>
+                <th className="border-b border-blue-gray-100 bg-blue-gray-50 py-2">
+                  <Typography className="font-extrabold opacity-70 text-indigo-800 text-2xl">
+                    Due date
+                  </Typography>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr
+                  key={task.taskId}
+                  className="bg-slate-100 hover:bg-slate-200"
+                >
+                  <td>
+                    <Typography
+                      variant="medium"
+                      className="font-semibold py-2 text-gray-700"
+                    >
+                      {task.description}
+                    </Typography>
+                  </td>
+                  <td>
+                    <Typography
+                      variant="medium"
+                      className="font-semibold py-2 text-gray-700"
+                    >
+                      {format(new Date(task.dueDate), "yyyy/MM/dd")}
+                    </Typography>
+                  </td>
+                  <td  className="px-auto">
+                    <Button
+                      className="bg-red-800 hover:bg-red-600 rounded-md h-8 py-1"
+                      onClick={() => handleDeleteTask(task.taskId)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </Card>
       </div>
     </div>
-  )
+  );
 }
 
 export default TaskManagement;
